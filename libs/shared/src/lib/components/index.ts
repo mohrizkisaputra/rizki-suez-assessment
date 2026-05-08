032860544b8973a6b783d4b375b10edbcdb4eec9
mohrizkisaputra <mrizkisaputra713@gmail.com>
2026-05-09 02:48:53 +0700

    [COM002] - create basic scaffolding / skeleton folder

---
+++ libs/shared/src/lib/components/index.ts
@@ -0,0 +1,2 @@
+export * from './tire-pressure-widget/tire-pressure-widget.component';
+export * from './speed-widget/speed-widget.component';
---
+++ libs/shared/src/lib/components/tire-pressure-widget/tire-pressure-widget.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-tire-pressure-widget',
+  standalone: true,
+  imports: [],
+  templateUrl: './tire-pressure-widget.component.html',
+  styleUrl: './tire-pressure-widget.component.css'
+})
+export class TirePressureWidgetComponent {
+
+}
